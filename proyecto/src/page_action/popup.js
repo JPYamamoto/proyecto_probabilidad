dfd0195744d4e4cf6668c5265d3df7843eb578b3
SoyOscarRH <soyoscarrh@gmail.com>
2021-01-28 12:55:41 -0600

    Show single results

--- proyecto/src/page_action/popup.js
+++ proyecto/src/page_action/popup.js
@@ -2,7 +2,22 @@ chrome.runtime.getBackgroundPage((page) => (console = page.console));
 
 chrome.runtime.send = chrome.runtime.sendMessage;
 document.get = document.getElementById;
-const [single, multiple] = ["extension_trigger_single", "extension_trigger_multiple"];
+const [single, multiple] = [
+  "extension_trigger_single",
+  "extension_trigger_multiple",
+];
 
-document.get("click-single-seller").onclick = () => chrome.runtime.send(single, console.log);
-document.get("click-multiple-sellers").onclick = () => chrome.runtime.send(multiple, console.log);
+document.get("click-single-seller").onclick = () =>
+  chrome.runtime.send(single, printSingle);
+document.get("click-multiple-sellers").onclick = () =>
+  chrome.runtime.send(multiple, console.log);
+
+const printSingle = ({ response }) => {
+  document.get("single-seller").innerHTML = `
+  <div class="result">
+    <p><b>Media: </b> ${response.media}</p>
+    <p><b>Mediana: </b> ${response.mediana}</p>
+    <p><b>Moda: </b> ${response.moda}</p>
+    <p><b>Numero de calificaciones: </b> ${response.num_calificaciones}</p>
+  </div>`;
+};
