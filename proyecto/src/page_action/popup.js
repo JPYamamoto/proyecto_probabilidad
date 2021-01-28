a11d3e21cca6d441bedf5102a35cacefdaf37ee0
SoyOscarRH <soyoscarrh@gmail.com>
2021-01-28 12:58:12 -0600

    forgot to run prettier

--- proyecto/src/page_action/popup.js
+++ proyecto/src/page_action/popup.js
@@ -2,15 +2,10 @@ chrome.runtime.getBackgroundPage((page) => (console = page.console));
 
 chrome.runtime.send = chrome.runtime.sendMessage;
 document.get = document.getElementById;
-const [single, multiple] = [
-  "extension_trigger_single",
-  "extension_trigger_multiple",
-];
+const [single, multiple] = ["extension_trigger_single", "extension_trigger_multiple"];
 
-document.get("click-single-seller").onclick = () =>
-  chrome.runtime.send(single, printSingle);
-document.get("click-multiple-sellers").onclick = () =>
-  chrome.runtime.send(multiple, console.log);
+document.get("click-single-seller").onclick = () => chrome.runtime.send(single, printSingle);
+document.get("click-multiple-sellers").onclick = () => chrome.runtime.send(multiple, console.log);
 
 const printSingle = ({ response }) => {
   document.get("single-seller").innerHTML = `
